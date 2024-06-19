e4e51b90555e34a87bbc6f0705759bfab31a9e90
quadcoX <support@quadcox.com>
2024-06-19 19:14:31 +0530

    added email submission

--- src/components/Contact_form.jsx
+++ src/components/Contact_form.jsx
@@ -1,33 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useFormspark } from '@formspark/use-formspark'
 import Email from '../assets/Email.svg'
 import Phone from '../assets/Phone.svg'
 
-const Contact_form = () => (
-    <>
+const Contact_form = () => {
+
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
+    const [email, setEmail] = useState('');
+    const [mob, setMob] = useState('');
+    const [msg, setMsg] = useState('');
+
+    const [submit, submitting] = useFormspark({
+    formId: "YfpxCczsK"
+  });
+
+
+
+  return (  <>
         <div className="contact-form-parent page-width">
             <div className="contact-form-wrapper">
                 <div className='contact-form-inner'>
                 <h2 className='form-heading basic-font'>Say <span>Hello.</span></h2>
                 <p className="form-description basic-font">We would love to talk to you, don't hesitate. Drop us a quick line.</p>
-                <form action="#" method='POST'>
+                <form onSubmit={async (e) => {
+        e.preventDefault();
+        await submit({ "First Name": fname, "Last Name": lname, "Email": email, "Phone": mob, "Message": msg});
+        setFname('');
+        setLname('');
+        setEmail('');
+        setMob('');
+        setMsg('');
+        alert('Message received! We will get back to you soon.');
+    }}>
                     <div className="form-field">
-                        <label className="hidden" for="fname">First name:</label>
-                        <input type="text" id="fname" name="fname" placeholder='First name' required />
+                        <label className="hidden" forHTML="fname">First name:</label>
+                        <input type="text" id="fname" name="fname" placeholder='First name' value={fname} onChange={(e) => setFname(e.target.value)} required />
                     </div>
                     <div className="form-field">
-                        <label className="hidden" for="lname">Last name:</label>
-                        <input type="text" id="lname" name="lname" placeholder='Last name' required />
+                        <label className="hidden" forHTML="lname">Last name:</label>
+                        <input type="text" id="lname" name="lname" placeholder='Last name' value={lname} onChange={(e) => setLname(e.target.value)} required />
                     </div>
                     <div className="form-field">
-                        <label className="hidden" for="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder='Email' required />
+                        <label className="hidden" forHTML="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} required />
                     </div>
                     <div className="form-field">
-                        <label className="hidden" for="mob">First name:</label>
+                        <label className="hidden" forHTML="mob" value={mob} onChange={(e) => setMob(e.target.value)}>Phone:</label>
                         <input type="number" id="mob" name="mob" placeholder='Phone' required />
                     </div>
                     <div className="form-field">
-                        <textarea type="text" id="msg" name="msg" placeholder='Message' required />
+                        <textarea type="text" id="msg" name="msg" placeholder='Message' value={msg} onChange={(e) => setMsg(e.target.value)} required />
+                    </div>
+                    <div className="form-field">
+                        <button className="submit-btn" type="submit" disabled={submitting}>Submit</button>
                     </div>
                 </form>
                 </div>
@@ -58,7 +84,7 @@ const Contact_form = () => (
                 </div>
             </div>
         </div>
-    </>
-)
+    </>);
+}
 
 export default Contact_form
